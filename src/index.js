ad251e6d8f8d1a3e9be24cafe1d1122bd4628245
pashax0 <pashax0@mail.ru>
2019-03-18 18:47:56 +0300

    fix: test 98 from 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let maxBase = base, v = 0, p = 1, n = 1, lastI = 0;
+  for (let i = 2; i <= maxBase; i++) {
+    if (maxBase % i == 0 && maxBase !== i) {
+      if (i == lastI) {
+        n++;
+      }
+      if (i > lastI) {
+        n = 1;
+      }
+      maxBase = maxBase / i;
+      lastI = i;
+      i = 1;
+    } else if (maxBase == i) {
+      if (i == lastI) {
+        n++;
+      }
+      if (i > lastI) {
+        n = 1;
+      }
+      break;
+    }
+  }
+  while (p <= number) {
+    p *= maxBase;
+    v += Math.floor(number / p);
+  }
+  return Math.floor(v / n);
 }
